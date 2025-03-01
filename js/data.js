bcba0dd81b2cb043870b8ecdd9a34b26f9511a48
Laura Moreno <chipifsdev@gmail.com>
2025-03-02 00:53:12 +0100

    seguir maquetando las cartas

--- js/data.js
+++ js/data.js
@@ -1,4 +1,13 @@
-
+const cartasCorrectas = [
+    {id: "carta1", nombre: 'Juanan', apodo: 'Bold', juego: 'pokemon'},
+    {id: "carta2", nombre: 'Chipi', apodo: 'La intensita', juego: 'sonic'},
+    {id: "carta3", nombre: 'Cora', apodo: 'Carpetitas', juego: 'pokemon'},
+    {id: "carta4", nombre: 'Javier', apodo: 'Iceman', juego: 'call of duty'},
+    {id: "carta5", nombre: 'Gian Carlo', apodo: 'El trampas', juego: 'dragon ball'},
+    {id: "carta6", nombre: 'Jean Franco', apodo: 'El yogurín', juego: 'call of duty'},
+    {id: "carta7", nombre: 'Mamady', apodo: 'Mamady', juego: 'dragon ball'},
+    {id: "carta8", nombre: 'Dani', apodo: 'Global ignore', juego: 'pokemon'},
+]
 
 
 
--- js/main.js
+++ js/main.js
@@ -1,80 +1,112 @@
-const nombre = document.querySelector('#nombre')
-const apodo = document.querySelector('#apodo')
-const form = document.querySelector('#formulario')
-const listName = document.querySelector('#listaNombres')
-
+const nombre = document.querySelector("#nombre");
+const apodo = document.querySelector("#apodo");
+const form = document.querySelector("#formulario");
+const listName = document.querySelector("#listaNombres");
 
 let cartasArray = JSON.parse(localStorage.getItem("cartas")) || [];
 
-
-
-function guardarEnLocalStorage(){
-    localStorage.setItem("cartas", JSON.stringify(cartasArray))
+function guardarEnLocalStorage() {
+  localStorage.setItem("cartas", JSON.stringify(cartasArray));
 }
 
-function cargarDesdeLocalStorage(){
+function cargarDesdeLocalStorage() {
+  listName.innerHTML = "";
 
-    listName.innerHTML= "";
-
-    cartasArray.forEach(({nombre, apodo}) => {
-        printOneCard(nombre, apodo, true)
-    });
+  cartasArray.forEach(({ nombre, apodo }) => {
+    printOneCard(nombre, apodo, true);
+  });
 }
-cargarDesdeLocalStorage()
+cargarDesdeLocalStorage();
 
-function printOneCard(nombre, apodo, fromLocalStorage = false){
-    const li = document.createElement("li")
-    li.classList.add("mi-estilo")
-    li.innerHTML =`El nombre que has elegido es <span>${nombre}</span> y su apodo es <span>${apodo}</span> <button class = "eliminar"> eliminar </button>`
+function printOneCard(nombre, apodo, fromLocalStorage = false) {
+  const li = document.createElement("li");
+  li.classList.add("mi-estilo");
+  li.innerHTML = `El nombre que has elegido es <span>${nombre}</span> y su apodo es <span>${apodo}</span> <button class = "eliminar"> eliminar </button>`;
 
-    listName.appendChild(li)
+  listName.appendChild(li);
 
-    li.querySelector('.eliminar').addEventListener('click', () => {
-        deleteOneCard(nombre, apodo, li)
-    })
+  li.querySelector(".eliminar").addEventListener("click", () => {
+    deleteOneCard(nombre, apodo, li);
+  });
 }
 
+function addOneCard(nombre, apodo, fromLocalStorage = false) {
+  if (
+    !fromLocalStorage &&
+    cartasArray.some(
+      (carta) => carta.nombre === nombre && carta.apodo === apodo
+    )
+  ) {
+    alert("A este personaje ya le pusiste ese apodo. Prueba con otro.");
+    return;
+  }
+  cartasArray.push({ nombre, apodo });
+  guardarEnLocalStorage();
+  printOneCard(nombre, apodo);
+  console.log(cartasArray);
 
-function addOneCard(nombre, apodo, fromLocalStorage = false){
-    if(!fromLocalStorage && cartasArray.some(carta => carta.nombre === nombre && carta.apodo === apodo )){
-        alert('A este personaje ya le pusiste ese apodo. Prueba con otro.');
-        return;
+  let cartaEncontrada = "";
+
+  if (
+    (cartaEncontrada = carta.find(
+      (carta) =>
+        carta.nombre.toLowerCase() === nombre.toLowerCase() &&
+        carta.apodo.toLowerCase() === apodo.toLowerCase()
+    ))
+  ) {
+    const cartaDefinitiva = document.getElementById(cartaEncontrada.id);
+    if (cartaDefinitiva) {
+      cartaDefinitiva.classList.add("volteada");
     }
-    cartasArray.push({nombre, apodo})
+  }
+}
+
+function addToListName(event) {
+  event.preventDefault();
+  const nombreValue = nombre.value;
+  const apodoValue = apodo.value;
+
+  if (nombreValue.trim() === "" || apodoValue.trim() === "") {
+    alert("¿De veras no sabes de quienes estamos hablando?");
+    return;
+  }
+  addOneCard(nombreValue, apodoValue);
+
+  form.reset();
+}
+
+form.addEventListener("submit", addToListName);
+
+function deleteOneCard(nombre, apodo, domElement) {
+  const index = cartasArray.findIndex(
+    (carta) => carta.nombre === nombre && carta.apodo === apodo
+  );
+  if (index !== -1) {
+    cartasArray.splice(index, 1);
     guardarEnLocalStorage();
-    printOneCard(nombre, apodo)
-    console.log(cartasArray)
+    console.log(cartasArray);
+  }
+  domElement.remove();
 }
 
+// const cartaColeccionable = cartasCorrectas.find(
+//   (carta) =>
+//     carta.nombre.toLowerCase() === nombreValue.toLowerCase() &&
+//     carta.apodo.toLowerCase() === apodoValue.toLowerCase()
+// );
 
-function addToListName(event){
-    event.preventDefault();
-    const nombreValue = nombre.value
-    const apodoValue = apodo.value
+// function cartaCorrecta(nombreValue, apodoValue) {
+//   return cartasCorrectas.find(
+//     (carta) => carta.nombre.toLowerCase() === nombreValue.toLowerCase() &&
+//     carta.apodo.toLowerCase() === apodoValue.toLowerCase()
+//   );
+// }
 
-    if(nombreValue === "" || apodoValue === ""){
-        alert("¿De veras no sabes de quienes estamos hablando?")
-        return
-    }
-    addOneCard(nombreValue, apodoValue)
-    
-    form.reset()
-}
+// function cartaColeccionada(carta){
+//     const cartasContainer = document.querySelector("#cartasContainer")
+//     const divCarta = document.createElement("div")
+//     divCarta.classList.add("carta")
 
-form.addEventListener('submit', addToListName)
-
-
-
-function deleteOneCard(nombre, apodo, domElement){
-    const index = cartasArray.findIndex(carta => carta.nombre === nombre && carta.apodo === apodo)
-    if(index !== -1){
-        cartasArray.splice(index, 1)
-        guardarEnLocalStorage()
-        console.log(cartasArray)
-    }
-    domElement.remove()
-
-}
-
-
-
+//     divCarta.innerHTML = `<h3>${carta.nombre}</h3><p>${carta.apodo}</p>`
+//     cartasContainer.appendChild(divCarta)
+// }
